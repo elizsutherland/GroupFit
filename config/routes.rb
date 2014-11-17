892e4de37703103943fdf6f7c95518491797f068
Elizabeth Collins <escollins27@gmail.com>
2014-11-17 14:22:08 -0500

    Users can view Dashboard and external links

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  root "groupfit#index"
+  root "dashboards#index"
 
   resource :session, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create]
-  resources :groupfit, only: [:new, :create]
 end
